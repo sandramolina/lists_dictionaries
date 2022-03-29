625dd4c88903b9bc3de259adeb784c013543d92c
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-03-29 18:39:13 +0100

    completes point 4

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -55,16 +55,20 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
-print(f'Jonnys twitter is {users["Jonathan"]["twitter"]}')
+print(f'Jonny\'s twitter is {users["Jonathan"]["twitter"]}')
 
 # 2. Get Erik's hometown
-print(f'Eriks hometown is {users["Erik"]["home_town"]}')
+print(f'Erik\'s hometown is {users["Erik"]["home_town"]}')
 
 # 3. Get the list of Erik's lottery numbers
-print(f'Eriks lottery numbers list is {users["Erik"]["lottery_numbers"]}')
+print(f'Erik\'s lottery numbers list is {users["Erik"]["lottery_numbers"]}')
 
 # 4. Get the species of Avril's pet Monty
+print(f'Avril\'s pet is a {users["Avril"]["pets"][0]["species"]}')
+
 # 5. Get the smallest of Erik's lottery numbers
+
+
 # 6. Return an list of Avril's lottery numbers that are even
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
